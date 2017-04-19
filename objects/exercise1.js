5fda238a847971e93ed8df4b42d031ab7b4189be
milaton <mila.iyibilek@gmail.com>
2017-04-19 23:06:25 +0200

    Update exercise1.js

--- objects/exercise1.js
+++ objects/exercise1.js
@@ -114,3 +114,206 @@ function departmentExperience(data){
   console.log(expYears);
 }
 departmentExperience(people);
+
+//Exercise 6: Make a new object which contains a number of employees in each department.
+function peopleInTotal(argument) {
+  var numberDepartment = {
+      IT: 0,
+      Engineering: 0,
+      Management: 0
+    };
+    for(var i = 0; i < argument.length; i++) {
+      if(argument[i].department == "IT") {
+        numberDepartment.IT += 1;
+      }
+      else if(argument[i].department == "Management"){
+        numberDepartment.Management += 1;
+      }
+      else {
+        numberDepartment.Engineering += 1;
+      }
+      
+    }
+  console.log(numberDepartment);
+}
+peopleInTotal(people);
+
+//Exercise 7: Make a new object which contains employees classified according to their experience. 
+function classExperience(argument) {
+  var expLevel = {
+    amateur: [],
+    newbie: [],
+    expert: [],
+    pro: []
+  };
+
+  for(var i = 0; i < argument.length; i++) {
+    if(argument[i].yearsExperience <= 2) {
+      expLevel.newbie.push(argument[i].name);
+    }
+    else if(argument[i].yearsExperience <=5) {
+      expLevel.amateur.push(argument[i].name);
+    }
+    else if(argument[i].yearsExperience <= 10) {
+      expLevel.expert.push(argument[i].name);
+    }
+    else if(argument[i].yearsExperience >= 10) {
+      expLevel.pro.push(argument[i].name);
+    }
+  }
+  console.log(expLevel);
+ }
+
+ classExperience(people);
+
+//Exercise 8: Classify employees according to their experience and return an object which contains a number of employees with same level experience.
+ function employeesAccordingToExperience(argument) {
+  var experienceLevel = {
+    amateur: 0,
+    newbie: 0,
+    expert: 0,
+    pro: 0
+  };
+
+  for(var i = 0; i < argument.length; i++) {
+    if(argument[i].yearsExperience <= 2) {
+      experienceLevel.newbie += 1;
+    }
+    else if(argument[i].yearsExperience <=5) {
+      experienceLevel.amateur += 1;
+    }
+    else if(argument[i].yearsExperience <= 10) {
+      experienceLevel.expert += 1;
+    }
+    else if(argument[i].yearsExperience >= 10) {
+      experienceLevel.pro += 1;
+    }
+  }
+  console.log(experienceLevel);
+ }
+
+ employeesAccordingToExperience(people);
+
+//Exercise 9: Get the names of the employees.
+function employeesNames(argument) {
+  var employed = [];
+
+  for(i = 0; i < argument.length; i++) {
+    employed.push(argument[i].name);
+  }
+console.log(employed);
+}
+employeesNames(people);
+
+//Exercise 10: How much is the grade greater than the limit number?
+function getNumberOfGradesGreaterThan(person, limit) {
+  var count = 0;
+  for(i = 0; i < person.marks.length; i++) {
+    if(person.marks[i] > limit) {
+        count += 1;
+    }
+  }
+  return count;
+}
+
+console.log("Grades greater than given number:");
+console.log(getNumberOfGradesGreaterThan(testObject, 5));
+console.log(getNumberOfGradesGreaterThan(testObject, 47));
+console.log(getNumberOfGradesGreaterThan(testObject, 3));
+
+
+//Exercise 11: New array is made of putting even nnumbers at the beginning of an array and odd at the end. Fuction returns an array. 
+
+function reorderGrades(grades) {
+  var evenNum = [];
+  var oddNum = [];
+  for(i = 0; i < grades.length; i++) {
+    if(grades[i] % 2 === 0) {
+        evenNum.push(grades[i]);
+    }
+    else if(grades[i] % 2 == 1 ){
+        oddNum.push(grades[i]);
+    }
+    }
+    return evenNum.concat(oddNum);
+}
+
+console.log("Reordered array:");
+console.log(reorderGrades(testObject.marks));
+
+// Exercise 12: Min grade of the person.
+function getMinGrade(person) {
+    var count = person.marks[0];
+    for(i = 0; i < person.marks.length; i++) {
+    
+            if(person.marks[i] < count) {
+               count = person.marks[i];
+            }
+     
+    }
+    return count;
+}
+
+console.log("Get Min Grade:");
+console.log(getMinGrade(testObject));
+
+
+// Exercise 13: Max grade of the person.
+
+function getMaxGrade(grades) {
+  var count = grades[0];
+  for(i = 0; i < grades.length; i++) {
+    if(grades[i] > count) {
+      count = grades[i];
+    }
+  }
+  return count;
+}
+
+
+console.log("Get Max Grade:");
+console.log(getMaxGrade(testObject.marks));
+
+
+// Exercise 14: New array is a group of even marks of a person.
+function getArrayOfEvenGrades(person) {
+  var newArr = [];
+  for(i = 0; i < person.marks.length; i++) {
+    if(person.marks[i] % 2 === 0) {
+      if(person.marks[i] === 0) {
+        continue;
+      }
+        newArr.push(person.marks[i]);
+    }
+  }
+  return newArr;
+}
+
+console.log("Array of Even Grades:");
+console.log(getArrayOfEvenGrades(testObject));
+
+// Exercise 15: New array returns odd grades.
+function getArrayOfOddGrades(grades) {
+var newArr = [];
+  for(i = 0; i < grades.length; i++) {
+    if(grades[i] % 2 == 1) {
+        newArr.push(grades[i]);
+    }
+  }
+  return newArr;
+}
+
+console.log("Array of Odd Grades:");
+console.log(getArrayOfOddGrades(testObject.marks));
+
+// Exercise 16: Sum of all grades, function returns a number.
+function getSumOfGrades(grades) {
+  var sum = 0;
+  for (var i = 0; i < grades.length; i++) {
+      sum += grades[i];
+  }
+  return sum;
+}
+
+console.log("Sum of elements:");
+console.log(getSumOfGrades(testObject.marks));
